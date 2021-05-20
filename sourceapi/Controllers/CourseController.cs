9ac5560df0352df967e04411078b79ca05025306
harrygupta89 <harrysist@gmail.com>
2021-05-21 03:06:30 +0530

    Add project files.

---
+++ sourceapi/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sourceapi.Services;
+
+namespace sourceapi.Controllers
+{
+    [ApiController]
+    [Route("/api/Course")]
+    public class CourseController : ControllerBase
+    {
+        private CourseService _course_service;
+
+        public CourseController(CourseService _svc)
+        {
+            _course_service = _svc;
+        }
+
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(_course_service.GetCourses());
+        }
+    }
+}
